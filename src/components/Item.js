d93f363e157fa1ad8084b7bed19dc8147a9e2b14
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-17 19:42:33 +0200

    wip

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,7 +8,7 @@ export default function Item(props) {
   // We get ALL items through props. We'll use the URL to find out which item is the one to show.
   const { items } = props
   
-  const params = useParams()
+  const  { id } = useParams()
 
   console.log(params)
 
