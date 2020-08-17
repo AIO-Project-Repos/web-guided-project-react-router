7edfee95894b48e8644b2e887f097eb80212b800
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-17 19:29:26 +0200

    wip

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,6 +8,8 @@ export default function Item(props) {
   // We get ALL items through props. We'll use the URL to find out which item is the one to show.
   const { items } = props
 
+  
+
   // 👉 STEP 7 - We need to pull item from items, using a parameter in the URL (:itemID)
   // Beware! The ids are integers, whereas URL parameters are strings.
   // Beware! The JSX is expecting 'item' to exist instantly!
